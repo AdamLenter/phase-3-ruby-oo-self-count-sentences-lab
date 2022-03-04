a91e599853fcec6d421847a46901d901695086af
Adam <adamlenter1@gmail.com>
2022-03-04 14:42:47 -0500

    commit tree d0542fe1b752ddf6cf44eea1bcf15ef6c835c5e3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[self.length - 1] == "."
   end
 
   def question?
-
+    self[self.length - 1] == "?"
   end
 
   def exclamation?
-
+    self[self.length - 1] == "!"
   end
 
   def count_sentences
+    sentence_array = self.split(/\.|!|\?/)
 
+    filtered_sentence_array = sentence_array.select do |b|
+      b != ""
+    end
+
+    filtered_sentence_array.length
   end
 end
